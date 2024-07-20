7f6345f7d208ec9290e5c4c4b1496f85d60c642f
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-07-20 16:35:02 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text
+        style={{ margin: 16, borderWidth: 1, borderColor: "red", padding: 16 }}
+      >
+        Hello World!
+      </Text>
+      <Button title="Tap Meeeeeeess!" />
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
